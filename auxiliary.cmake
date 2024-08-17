1530f15bef2aab97d31e4c2b72bfe46796cfed68
Marcus Welander <mwthinker@yahoo.se>
2024-08-17 14:18:17 +0200

    Fix extract hash from source provided

--- auxiliary.cmake
+++ auxiliary.cmake
@@ -1,9 +1,9 @@
 # Extracts the git hash in the SOURCE_DIR and stores it in OUT_VAR
 function(extract_git_hash SOURCE_DIR OUT_VAR)
 	if (WIN32)
-		execute_process(COMMAND cmd /C "git -C ${CMAKE_CURRENT_SOURCE_DIR} rev-parse HEAD" OUTPUT_VARIABLE VALUE OUTPUT_STRIP_TRAILING_WHITESPACE)
+		execute_process(COMMAND cmd /C "git -C ${SOURCE_DIR} rev-parse HEAD" OUTPUT_VARIABLE VALUE OUTPUT_STRIP_TRAILING_WHITESPACE)
 	else ()
-		execute_process(COMMAND bash -c "git -C ${CMAKE_CURRENT_SOURCE_DIR} rev-parse HEAD" OUTPUT_VARIABLE VALUE OUTPUT_STRIP_TRAILING_WHITESPACE)
+		execute_process(COMMAND bash -c "git -C ${SOURCE_DIR} rev-parse HEAD" OUTPUT_VARIABLE VALUE OUTPUT_STRIP_TRAILING_WHITESPACE)
 	endif ()
 	set(${OUT_VAR} "${VALUE}" PARENT_SCOPE)
 endfunction()
